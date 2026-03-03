2f80131f4b91a821231038f503519053df4130bc
Luis Fernando <105691753+Luis1f2@users.noreply.github.com>
2026-03-02 19:13:22 -0600

    Delete src/professional_profiles directory
    Se borro por motivos de que no se solicito para las pruebas conjuntas

--- src/professional_profiles/domain/repository/ProfessionalProfilesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { ProfessionalProfile } from "../entities/Professionalprofiles";
-
-export interface ProfessionalProfileRepository {
-  save(profile: ProfessionalProfile): Promise<void>;
-  findByUserId(userId: string): Promise<ProfessionalProfile | null>;
-  deleteByUserId(userId: string): Promise<void>;
-}
